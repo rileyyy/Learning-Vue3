eba09850bd74a432a967938a765f72d0f26a8da9
Riley Schroeder <rileyyy@live.com>
2024-10-27 14:46:55 +0100

    Example - eventss and methods

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const app = Vue.createApp({
       author: 'George Orwell',
       age: 45,
     }
+  },
+  methods: {
+    increment() {
+      this.age += 1;
+    },
+    decrement() {
+      this.age -= 1;
+    },
+    updateTitle(title) {
+      this.title = title;
+    }
   }
+
 });
 app.mount('#app');
