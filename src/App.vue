d4a010a1246d31c70f6e5f0da1265a75dc985252
Riley Schroeder <rileyyy@live.com>
2024-10-27 16:57:58 +0100

    Example - emit custom events

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>{{title}}</h1>
-  <Modal :header="header" :text="text" theme="sale"/>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @closeModal="toggleModal"/>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
@@ -15,9 +19,15 @@ export default {
     return {
       title: 'Welcome to Your Vue.js App',
       header: 'Sign up for the giveaway!',
-      text: 'Grab your ninja swag at a discount!'
+      text: 'Grab your ninja swag at a discount!',
+      showModal: false,
     }
   },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
