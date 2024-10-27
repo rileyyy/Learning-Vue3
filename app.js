b5003d9f47e59d5e41e0a3ea6406250f50a773bf
Riley Schroeder <rileyyy@live.com>
2024-10-27 15:43:27 +0100

    Example - computed properties

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ const app = Vue.createApp({
     toggleFavorite(book) {
       book.isFav = !book.isFav;
     }
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
+    }
   }
 
 });
