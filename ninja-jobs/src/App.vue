e81185bcf043fafe237c7a4c39a2b1dc36ad368f
Riley Schroeder <rileyyy@live.com>
2024-10-28 14:52:33 +0100

    Add Vue routing

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link :to="{name: 'About'}">About</router-link> |
-    <router-link :to="{name: 'Jobs'}">Jobs</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link> |
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </nav>
-  <router-view/>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
+  <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  }
+}
+</script>
 
 <style>
 #app {
@@ -27,5 +48,12 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+button {
+  margin: 0 12px;
+  padding: 12px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
