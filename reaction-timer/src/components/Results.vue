bdaa4a1808b7e49cdf00399037b60b0fba7a79f7
Riley Schroeder <rileyyy@live.com>
2024-10-28 12:32:40 +0100

    Stylize and add ranking

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,12 +1,30 @@
 <template>
   <p>Score: {{ score }} ms</p>
+  <p class="rank">Rank: {{ rank }}</p>
 </template>
 
 <script>
 export default {
   props: ['score'],
+  data() {
+    return {
+      rank: null,
+    }
+  },
+  mounted() {
+    this.score < 300
+      ? this.rank = 'Ninja'
+      : this.score < 400
+        ? this.rank = 'Jedi'
+        : this.rank = 'Padawan';
+  },
 }
 </script>
 
 <style>
+.rank {
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: #0faf87;
+}
 </style>
