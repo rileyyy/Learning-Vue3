1208a2cd0ad88d4b2a6c5efc4d5751a6361b54ff
Riley Schroeder <rileyyy@live.com>
2024-10-28 12:25:51 +0100

    Challenge - Add Results components

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @gameOverEvent="endGame"/>
+  <Results v-if="score" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 
 export default {
   name: 'App',
   components: {
     Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
     }
   },
   methods: {
     start() {
       this.isPlaying = true;
+      this.score = null;
       this.delay = 2000 + (Math.random() * 3000);
-    }
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false;
+      this.score = reactionTime;
+    },
   },
 }
 </script>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Score: {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+}
+</script>
+
+<style>
+</style>
