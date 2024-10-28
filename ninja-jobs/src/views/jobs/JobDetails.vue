b863e58b7097706c2fc68936227bef0e285e40e8
Riley Schroeder <rileyyy@live.com>
2024-10-28 16:14:54 +0100

    Add data loading and handle pre-load state

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1>Job Details</h1>
-  <p>Job id: {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>Job id: {{ id }}</p>
+    <p>Description: {{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ['id'],
+  data() {
+    return {
+      job: null,
+    }
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+    .then(response => response.json())
+    .then(data => this.job = data)
+    .catch(error => console.log(error));
+  }
 }
 </script>
 
