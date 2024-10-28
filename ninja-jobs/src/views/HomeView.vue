63158174969a1f24c52394980b10426623ddd7c3
Riley Schroeder <rileyyy@live.com>
2024-10-28 14:18:01 +0100

    Scaffold in Vue router project

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quibusdam accusamus voluptatibus nemo, provident exercitationem dicta at mollitia corporis fugit laudantium dolorum est et maiores repellat distinctio atque excepturi delectus reprehenderit itaque hic nostrum voluptatem. Quasi omnis iusto mollitia nostrum! A deleniti ex, adipisci sint maxime voluptatibus eligendi, vero error incidunt hic alias eveniet deserunt laboriosam vitae explicabo quibusdam dignissimos illum, dolores sunt maiores commodi provident obcaecati labore minus. Sed voluptatem quia corporis cumque sequi veniam earum. Neque vero molestiae ipsa alias rerum harum similique adipisci sit animi nihil error doloribus cum, ut, iusto accusantium porro atque molestias tempora culpa!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quibusdam accusamus voluptatibus nemo, provident exercitationem dicta at mollitia corporis fugit laudantium dolorum est et maiores repellat distinctio atque excepturi delectus reprehenderit itaque hic nostrum voluptatem. Quasi omnis iusto mollitia nostrum! A deleniti ex, adipisci sint maxime voluptatibus eligendi, vero error incidunt hic alias eveniet deserunt laboriosam vitae explicabo quibusdam dignissimos illum, dolores sunt maiores commodi provident obcaecati labore minus. Sed voluptatem quia corporis cumque sequi veniam earum. Neque vero molestiae ipsa alias rerum harum similique adipisci sit animi nihil error doloribus cum, ut, iusto accusantium porro atque molestias tempora culpa!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
