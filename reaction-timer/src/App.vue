d639095ffa2b3043cabfac5c7b8e2d70af04a3a3
Riley Schroeder <rileyyy@live.com>
2024-10-28 12:00:44 +0100

    Add and implement Block component basis

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from './components/Block.vue';
+
 export default {
   name: 'App',
   components: {
-  }
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + (Math.random() * 3000);
+    }
+  },
 }
 </script>
 
