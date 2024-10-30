e7499aeacd0baacb4c45128c6443d286b54ea132
Riley Schroeder <rileyyy@live.com>
2024-10-30 12:48:49 +0100

    Add posts compents

---
+++ dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post">
+    <h2>{{ post.title }}</h2>
+    <p>{{ snipet }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'SinglePost',
+  props: ['post'],
+  setup(props) {
+    const snipet = computed(() => {
+      return props.post.body.slice(0, 100) + '...';
+    });
+
+    return { snipet };
+  }
+}
+</script>
--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import PostList from '../components/PostList.vue';
+
 export default {
   name: 'HomeView',
+  components: {
+    PostList,
+  },
+  setup() {
+    const posts = ref([
+      {title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat facere tempore culpa, quo amet esse ea ipsum iste illo libero numquam voluptas quam nihil vitae eligendi labore molestias obcaecati aliquid nobis officia natus commodi. Molestias, nobis expedita nostrum inventore beatae quibusdam libero corrupti distinctio repudiandae perferendis quisquam, est, ea dolore impedit dignissimos iste atque fugit mollitia corporis facilis itaque assumenda? Modi reiciendis quibusdam ad maiores, nesciunt magni totam asperiores corporis praesentium deleniti nam voluptate optio veniam odio fuga earum autem quidem repellendus quae at vel culpa. Aspernatur magnam magni vel dolorem minima atque laboriosam voluptates libero molestias labore, beatae cum.', id: 1},
+      {title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2},
+    ]);
+
+    return { posts };
+  }
 }
 </script>
